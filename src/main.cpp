e44f55b75e5cd62a8e089ebf6af6d5af411f2410
Gurvir Singh <gurvirsingh.dev@gmail.com>
2026-01-18 22:44:57 +0000

    ability to read source file

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,48 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 #include "base/Lexer.cpp"
 
-int main()
+/**
+ * Function to read the give source file and
+ * return its contents as a string.
+ */
+std::string readSourceFile(const std::string &filePath)
 {
-  Lexer lexer(
-      "int main() {\n"
-      "  # This is a comment\n"
-      "  float number = 3.14;\n"
-      "}");
-  auto tokens = lexer.tokenize();
+  std::ifstream fileStream(filePath);
+  if (!fileStream.is_open())
+    throw std::runtime_error("Could not open file: " + filePath);
 
-  for (const auto &token : tokens)
+  std::stringstream buffer;
+  buffer << fileStream.rdbuf();
+  return buffer.str();
+}
+
+int main(int argc, char *argv[])
+{
+  /**
+   * Asserting correct number of arguments are provided.
+   */
+  if (argc != 2)
   {
-    std::cout << token.toJson().dump() << std::endl;
+    std::cerr << "Usage: " << argv[0] << " <source-file-path>" << std::endl;
+    return 1;
   }
+
+  /**
+   * Reading the source file and tokenizing its contents.
+   */
+  std::string sourceFilePath = argv[1];
+  std::string sourceCode = readSourceFile(sourceFilePath);
+  Lexer lexer(sourceCode);
+  std::vector<Token> tokens = lexer.tokenize();
+
+  /**
+   * Outputting the tokens as JSON.
+   */
+  for (const auto &token : tokens)
+    std::cout << token.toJson().dump() << std::endl;
   return 0;
 }
